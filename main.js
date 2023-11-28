bdde509a70c08598571d0d65b2ab29de838687fd
Piotr Poterek <peterpoterek0@gmail.com>
2023-11-28 17:48:12 +0100

    card toggle icon

--- main.js
+++ main.js
@@ -10,14 +10,17 @@ const fruitPairs = fruits.flatMap((fruit) => {
 const shuffledfruits = fruitPairs.sort(() => Math.random() - 0.5);
 
 const cards = document.querySelectorAll("#card");
+
 cards.forEach((card, i) => {
   const cardContent = document.createElement("span");
+
+  cardContent.classList.add("opacity-0", "pointer-events-none");
   cardContent.innerHTML = shuffledfruits[i];
 
   card.append(cardContent);
 });
 container.addEventListener("click", (e) => {
   if (e.target.id === "card") {
-    console.log(e.target.innerHTML);
+    e.target.querySelector("span").classList.toggle("opacity-0");
   }
 });
