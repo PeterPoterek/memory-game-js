761c64151b8eccd0ef6c4c28e6d3953953147933
Piotr Poterek <peterpoterek0@gmail.com>
2023-11-28 16:26:07 +0100

    shuffle cards

--- main.js
+++ main.js
@@ -1,7 +1,20 @@
 const container = document.querySelector("#container");
 
+const fruits = ["🍎", "🍐", "🍋", "🍌", "🍇", "🍊"];
+
+const shuffleFruits = () => {};
+const fruitPairs = fruits.flatMap((fruit) => {
+  return [fruit, fruit];
+});
+
+const shuffledfruits = fruitPairs.sort(() => Math.random() - 0.5);
+
+const cards = document.querySelectorAll("#card");
+cards.forEach((card, i) => {
+  card.innerHTML = shuffledfruits[i];
+});
 container.addEventListener("click", (e) => {
-  if (e.target.id === "guess") {
-    console.log("guess clicked");
+  if (e.target.id === "card") {
+    console.log(e.target.innerHTML);
   }
 });
