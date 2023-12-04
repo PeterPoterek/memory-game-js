81bfef146ef18ede8175486f59baeab9f1278ad0
Piotr Poterek <peterpoterek0@gmail.com>
2023-12-04 01:12:00 +0100

    compare cards

--- main.js
+++ main.js
@@ -11,16 +11,58 @@ const shuffledfruits = fruitPairs.sort(() => Math.random() - 0.5);
 const cardContainer = document.querySelector(".cardContainer");
 const cards = document.querySelectorAll(".card");
 
+let currentPairCheck = [];
+
 cards.forEach((card, i) => {
   const front = card.querySelector(".front");
   front.textContent = shuffledfruits[i];
-  // card.classList.toggle("rotate");
 });
 
-cardContainer.addEventListener("click", (event) => {
-  const clickedCard = event.target.closest(".card");
+const handleCardsSame = (currentPairCheck) => {
+  const [firstCard, secondCard] = currentPairCheck;
+
+  const firstCardIcon = firstCard.children[0].textContent;
+  const secondCardIcon = secondCard.children[0].textContent;
+
+  console.log(`${firstCardIcon} ${secondCardIcon} - Same`);
+};
+const handleCardsDiffrent = (currentPairCheck) => {
+  const [firstCard, secondCard] = currentPairCheck;
+
+  const firstCardIcon = firstCard.children[0].textContent;
+  const secondCardIcon = secondCard.children[0].textContent;
+
+  console.log(`${firstCardIcon} ${secondCardIcon} - Diffrent`);
+};
+
+cardContainer.addEventListener("click", (e) => {
+  const clickedCard = e.target.closest(".card");
 
   if (clickedCard) {
-    clickedCard.classList.toggle("rotate");
+    if (currentPairCheck.length < 2) {
+      // 1 card is selected
+      clickedCard.classList.toggle("rotate");
+      currentPairCheck.push(clickedCard);
+
+      clickedCard.classList.add("pointer-events-none");
+    }
+
+    if (currentPairCheck.length === 2) {
+      //2 cards are selected - compare
+
+      const firstCard = currentPairCheck[0].children[0].textContent;
+      const secondCard = currentPairCheck[1].children[0].textContent;
+
+      firstCard === secondCard ? handleCardsSame(currentPairCheck) : handleCardsDiffrent(currentPairCheck);
+    }
   }
 });
+
+setInterval(() => {
+  if (currentPairCheck[0]) {
+    console.log(currentPairCheck[0].children[0].textContent);
+  }
+  if (currentPairCheck[1]) {
+    console.log(currentPairCheck[1].children[0].textContent);
+  }
+}, 1000);
