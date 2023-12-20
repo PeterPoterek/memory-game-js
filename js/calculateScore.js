944cf4cb67ada1cc5231d6b9671a49140f74ddf9
Piotr Poterek <peterpoterek0@gmail.com>
2023-12-20 21:50:26 +0100

    Update calculateScore.js

--- js/calculateScore.js
+++ js/calculateScore.js
@@ -2,7 +2,11 @@ const timeWeight = 0.5;
 const wrongGuessWeight = 3.5;
 
 const calculateScore = (timeElapsedInSeconds, wrongGuesses) => {
-  return 100 - (timeElapsedInSeconds * timeWeight + wrongGuesses * wrongGuessWeight);
+  let score = 100 - (timeElapsedInSeconds * timeWeight + wrongGuesses * wrongGuessWeight);
+
+  score = Math.max(score, 0);
+
+  return score;
 };
 
 export default calculateScore;
